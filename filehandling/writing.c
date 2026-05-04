c8093a3fa33c41b7d5f4c98bad5efd2989d273b1
rootvector <rootvector.sec@gmail.com>
2026-05-04 22:14:34 +0500

    writing into file using argv

--- filehandling/writing.c
+++ filehandling/writing.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
     FILE *fp;
-    fp = fopen("sample.txt", "w");
+    fp = fopen("sample.txt", "a");
     if(fp == NULL){
         ferror(fp);
         return -1;
     }
-    
-    fprintf(fp, "this is a text\n");
-    fprintf(fp, "this is next line\n");
+    int i=1;
+    while(argc-- > 0){
+        fprintf(fp, argv[i]);
+        i++;
+    }
+
+    fprintf(fp, "\n");
 
     fclose(fp);
     return 0;
