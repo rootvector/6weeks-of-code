a61d6af05eb3c4b1d9ef8ecf71840b3d243c7e74
rootvector <rootvector.sec@gmail.com>
2026-05-10 14:40:20 +0530

    square edge points

---
+++ linuxsyscall/terminalcontrols/cube.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "include/tercont.h"
+
+#define WIDTH 400
+#define HEIGGT 250
+
+int main(){
+	int x = 30, y = 30, z = 30;
+	int xb = x/z, yb = y/z;
+
+	CLEAR_SCR(2);
+	HIDE_CURSOR();
+
+	MOVE_TO(x, y);
+	printf("%s#%s", RED, NOR);
+	MOVE_TO((x+20),y);
+	printf("%s#%s", RED, NOR);
+	MOVE_TO(x, (y+20));
+	printf("%s#%s", RED, NOR);
+	MOVE_TO((x+20),(y+20));
+	printf("%s#%s", RED, NOR);
+	
+	MOVE_TO(xb, yb);                                                  printf("%s#%s", RED, NOR);                                        MOVE_TO((xb+20),yb);                                              printf("%s#%s", RED, NOR);                                        MOVE_TO(xb, (yb+20));                                             printf("%s#%s", RED, NOR);                                        MOVE_TO((xb+20),(yb+20));                                         printf("%s#%s", RED, NOR);
+
+	printf("\n\n");
+	SHOW_CURSOR();
+
+	return 0;
+}
